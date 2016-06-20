25a8a75fcea6ba23b20217e9f2f94126b6095701
Matthew Stroud <kicker69101@gmail.com>
2016-06-19 20:43:30 -0600

    Models in place

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from frontend.models import *
 
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Address)
--- frontend/models.py
+++ frontend/models.py
@@ -2,4 +2,50 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    birthday = models.DateField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    picture = models.ImageField(blank=True, null=True)
+    phone = models.CharField(max_length=12, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Address(models.Model):
+    Person = models.ForeignKey('Person')
+    Street = models.CharField(max_length=255)
+    City = models.CharField(max_length=255)
+    Zip = models.CharField(max_length=20)
+    State = models.CharField(max_length=255)
+    Country = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "(%s) %s" % (self.Person.name, self.Street)
+
+
+class LikesDislike(models.Model):
+    Person = models.ManyToManyField('Person')
+    LIKE = 'like'
+    DISLIKE = 'dislike'
+    DIRECTIONS = (
+        (LIKE, 'like'),
+        (DISLIKE, 'dislike'),
+    )
+    direction = models.CharField(max_length=20, choices=DIRECTIONS)
+    subject = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.subject, self.direction)
+
+
+class PersonNotes(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    Person = models.ForeignKey('Person')
+    note = models.TextField()
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.Person.name, self.timestamp)
